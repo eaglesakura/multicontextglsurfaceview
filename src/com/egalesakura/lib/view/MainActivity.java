53e0cd76feeed7f5f5da02c66e2e088c39aa25fa
eaglesakura <eagle.sakura@gmail.com>
2013-12-26 15:17:54 +0900

    MultiContext対応のGLSurfaceViewを作成

---
+++ src/com/egalesakura/lib/view/MainActivity.java
@@ -0,0 +1,139 @@
+package com.egalesakura.lib.view;
+
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.GLUtils;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+    MultiContextGLSurfaceView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        view = new MultiContextGLSurfaceView(this);
+
+        {
+            view.setEGLContextClientVersion(1);
+            view.setRenderer(rendererGL11);
+        }
+
+        setContentView(view);
+    }
+
+    Bitmap loadImage() {
+        return BitmapFactory.decodeResource(getResources(), R.drawable.cat);
+    }
+
+    Buffer wrap(float[] buffer) {
+        ByteBuffer bb = ByteBuffer.allocateDirect(buffer.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        FloatBuffer fb = bb.asFloatBuffer();
+        fb.put(buffer);
+        fb.position(0);
+        return fb;
+    }
+
+    private GLSurfaceView.Renderer rendererGL11 = new Renderer() {
+        int asyncLoadedTextureId = 0;
+
+        @Override
+        public void onSurfaceCreated(final GL10 gl, EGLConfig config) {
+            view.requestAsyncGLEvent(new Runnable() {
+                @Override
+                public void run() {
+                    // async GL Thread
+
+                    try {
+                        // test sleep 
+                        Thread.sleep(5000);
+                    } catch (Exception e) {
+
+                    }
+
+                    int[] tex = new int[1];
+                    gl.glGenTextures(1, tex, 0);
+
+                    final int texId = tex[0];
+                    gl.glEnable(GL10.GL_TEXTURE_2D);
+                    gl.glBindTexture(GL10.GL_TEXTURE_2D, texId);
+                    GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, loadImage(), 0);
+                    gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
+                    gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_NEAREST);
+
+                    asyncLoadedTextureId = texId;
+
+                    Log.i("Async", "Texture Loaded :: " + texId);
+                }
+            });
+        }
+
+        private int screenWidth;
+        private int screenHeight;
+
+        @Override
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+            this.screenWidth = width;
+            this.screenHeight = height;
+            gl.glViewport(0, 0, width, height);
+        }
+
+        private void drawQuad(GL10 gl10, int x, int y, int w, int h) {
+            float left = ((float) x / (float) screenWidth) * 2.0f - 1.0f;
+            float top = ((float) y / (float) screenHeight) * 2.0f - 1.0f;
+            float right = left + ((float) w / (float) screenWidth) * 2.0f;
+            float bottom = top + ((float) h / (float) screenHeight) * 2.0f;
+            // ! 上下を反転させる
+            top = -top;
+            bottom = -bottom;
+            // ! 位置情報
+            float positions[] = { // ! x y z
+                    left, top, 0.0f, //
+                    left, bottom, 0.0f, // !< 左下 
+                    right, top, 0.0f, // !< 右上 
+                    right, bottom, 0.0f, // !< 右下
+            };
+            gl10.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+            gl10.glVertexPointer(3, GL10.GL_FLOAT, 0, wrap(positions));
+            gl10.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
+        }
+
+        @Override
+        public void onDrawFrame(GL10 gl) {
+            gl.glClearColor(0, 1, (float) Math.random(), 1);
+            gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+            if (asyncLoadedTextureId != 0) {
+                // async load completed
+                gl.glEnable(GL10.GL_TEXTURE_2D);
+                gl.glBindTexture(GL10.GL_TEXTURE_2D, asyncLoadedTextureId);
+
+                float uv[] = {
+                        0.0f, 0.0f, // !< 左上
+                        0.0f, 1.0f, // !< 左下
+                        1.0f, 0.0f, // !< 右上
+                        1.0f, 1.0f, // !< 右下
+                };
+
+                gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+                gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, wrap(uv));
+
+            }
+            drawQuad(gl, 0, 0, 512, 512);
+        }
+    };
+}
---
+++ src/com/egalesakura/lib/view/MultiContextGLSurfaceView.java
@@ -0,0 +1,211 @@
+package com.egalesakura.lib.view;
+
+import static javax.microedition.khronos.egl.EGL10.*;
+
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.egl.EGLSurface;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+public class MultiContextGLSurfaceView extends GLSurfaceView {
+
+    /**
+     * EGLContextの参照数
+     */
+    private int mEGLContextRef = 0;
+
+    /**
+     * SharedContextのメイン用
+     */
+    private EGLContext mMasterContext = null;
+
+    /**
+     * EGLContext操作時のロック
+     */
+    private Object mEGLContextLock = new Object();
+
+    /**
+     * def OpenGL ES 1.x
+     */
+    private int mEglClientVersion = 1;
+
+    private EGL10 mEGL = null;
+
+    private EGLConfig mEGLConfig = null;
+
+    private EGLDisplay mEGLDisplay = null;
+
+    public MultiContextGLSurfaceView(Context context) {
+        super(context);
+    }
+
+    public MultiContextGLSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /**
+     * このメソッドは必ず呼び出されるため、加工して独自Factoryをセットアップする
+     */
+    @Override
+    public void setRenderer(Renderer renderer) {
+        setEGLContextFactory(mEGLContextFactory);
+        super.setRenderer(renderer);
+    }
+
+    /**
+     * set OpenGL ES Version.
+     */
+    @Override
+    public void setEGLContextClientVersion(int version) {
+        super.setEGLContextClientVersion(version);
+        this.mEglClientVersion = version;
+    }
+
+    /**
+     * GL動作を非同期で行う
+     * Textureの読み込み等を想定
+     * @param event
+     */
+    public void requestAsyncGLEvent(final Runnable event) {
+        Thread thread = (new Thread() {
+            @Override
+            public void run() {
+                EGLContext context = newSlaveContext();
+                EGLSurface surface = newDummySurface();
+
+                try {
+                    mEGL.eglMakeCurrent(mEGLDisplay, surface, surface, context);
+
+                    // call event
+                    event.run();
+                } finally {
+                    mEGL.eglMakeCurrent(mEGLDisplay, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+
+                    destroySlaveContext(context);
+                    destroyDummySurface(surface);
+                }
+            }
+        });
+
+        thread.setName("GL-Background");
+        thread.start();
+
+    }
+
+    /**
+     * 用途に合わせたEGLContext attributeを生成する
+     * @return
+     */
+    private int[] getContextAttributes() {
+        if (mEglClientVersion == 1) {
+            return null;
+        } else {
+            return new int[] {
+                    0x3098 /* EGL_CONTEXT_CLIENT_VERSION */, mEglClientVersion, EGL_NONE
+            };
+        }
+    }
+
+    /**
+     * マスターアクセス用Contextを取得する
+     * @return
+     */
+    private EGLContext getMasterContext(EGL10 egl, EGLDisplay display, EGLConfig config) {
+        synchronized (mEGLContextLock) {
+            if (mMasterContext == null) {
+                mMasterContext = egl.eglCreateContext(display, config, EGL_NO_CONTEXT, getContextAttributes());
+
+                this.mEGL = egl;
+                this.mEGLDisplay = display;
+                this.mEGLConfig = config;
+            }
+            return mMasterContext;
+        }
+    }
+
+    /**
+     * EGLの参照数を追加する
+     */
+    private void retainEGL() {
+        synchronized (mEGLContextLock) {
+            ++mEGLContextRef;
+        }
+    }
+
+    /**
+     * EGLの参照数を減らす
+     */
+    private void releaseEGL() {
+        synchronized (mEGLContextLock) {
+            --mEGLContextRef;
+
+            if (mEGLContextRef == 0) {
+                // マスターContextが不要になった
+                mEGL.eglDestroyContext(mEGLDisplay, mMasterContext);
+            }
+        }
+    }
+
+    /**
+     * eglMakeCurrent用のDummyを作成する
+     * @return
+     */
+    public EGLSurface newDummySurface() {
+        return mEGL.eglCreatePbufferSurface(mEGLDisplay, mEGLConfig, new int[] {
+                //
+                EGL_WIDTH, 1, EGL_HEIGHT, 1,
+                //
+                EGL_NONE,
+        });
+    }
+
+    /**
+     * eglMakeCurrent用のdummyを廃棄する
+     * @param surface
+     */
+    public void destroyDummySurface(EGLSurface surface) {
+        mEGL.eglDestroySurface(mEGLDisplay, surface);
+    }
+
+    /**
+     * スレイブContextを生成する
+     * @return
+     */
+    public EGLContext newSlaveContext() {
+        return mEGLContextFactory.createContext(mEGL, mEGLDisplay, mEGLConfig);
+    }
+
+    /**
+     * スレイブContextを廃棄する
+     * @param context
+     */
+    public void destroySlaveContext(EGLContext context) {
+        mEGLContextFactory.destroyContext(mEGL, mEGLDisplay, context);
+    }
+
+    /**
+     * default factory
+     */
+    private EGLContextFactory mEGLContextFactory = new EGLContextFactory() {
+
+        @Override
+        public void destroyContext(EGL10 egl, EGLDisplay display, EGLContext context) {
+            egl.eglDestroyContext(display, context);
+            releaseEGL();
+        }
+
+        @Override
+        public EGLContext createContext(EGL10 egl, EGLDisplay display, EGLConfig eglConfig) {
+            EGLContext master = getMasterContext(egl, display, eglConfig);
+            EGLContext result = egl.eglCreateContext(display, eglConfig, master, getContextAttributes());
+            retainEGL();
+            return result;
+        }
+    };
+
+}
